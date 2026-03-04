e0f67374326889a7188c3662426f49863a622d9d
R_hmt dev <rohmatulloh.3609@gmail.com>
2026-03-04 21:36:03 +0700

    Update home.js

--- api/home.js
+++ api/home.js
@@ -1,13 +1,6 @@
 const { melolohome } = require('./helpers/scraper');
-
 export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET');
-
-    try {
-        const data = await melolohome();
-        res.status(200).json({ success: true, data });
-    } catch (error) {
-        res.status(500).json({ success: false, message: error.message });
-    }
-} 
+    try { res.status(200).json({ success: true, data: await melolohome() }); } 
+    catch (e) { res.status(500).json({ success: false, message: e.message }); }
+}
